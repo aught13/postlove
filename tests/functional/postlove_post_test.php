eb7558f06b69f8cde086c56254bee1de8ab9286e
aught13 <william.bill.guy@gmail.com>
2017-09-12 21:18:46 -0400

    another

--- tests/functional/postlove_post_test.php
+++ tests/functional/postlove_post_test.php
@@ -58,16 +58,16 @@ class postlove_post_test extends postlove_base
 	
 	public function test_guest_see_loves()
 	{
-		$crawler = self::request('GET', "viewtopic.php?f=2&t=2");
-		$this->assertContains('1', $crawler->filter('#p3')->filter('.postlove_likers')->filter('span')->attr('title'));
+		$crawler = self::request('GET', "viewtopic.php?f=2&t=1");
+		$this->assertContains('1', $crawler->filter('#p2')->filter('.postlove_likers')->filter('span')->attr('title'));
 	}
 	
 	public function test_guests_cannot_like()
 	{
 		$crw1 = self::request('GET', 'app.php/postlove/toggle/3', array(), array(), array('CONTENT_TYPE'	=> 'application/json'));
 		
-		$crawler = self::request('GET', "viewtopic.php?f=2&t={$this->tpic}");
-		$this->assertContains('1', $crawler->filter('#p' . $this->pst)->filter('.postlove_likers')->filter('span')->attr('title'));
+		$crawler = self::request('GET', "viewtopic.php?f=2&t=2");
+		$this->assertContains('1', $crawler->filter('#p3' . $this->pst)->filter('.postlove_likers')->filter('span')->attr('title'));
 		
 	}
 	public function test_show_likes_given()
